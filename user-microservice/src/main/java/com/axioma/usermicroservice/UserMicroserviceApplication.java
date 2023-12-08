8de6be640f4eb9a2e5bce7f7727625eb346574dc
RafaelTalavera <rtalavera@live.com.ar>
2023-12-08 15:38:07 -0300

    configuration eureka client

--- user-microservice/src/main/java/com/axioma/usermicroservice/UserMicroserviceApplication.java
+++ user-microservice/src/main/java/com/axioma/usermicroservice/UserMicroserviceApplication.java
@@ -3,6 +3,8 @@ package com.axioma.usermicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class UserMicroserviceApplication {
 
