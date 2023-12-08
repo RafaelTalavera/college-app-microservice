3f9cfcc6228925ed61322525050784f8f532f60b
RafaelTalavera <rtalavera@live.com.ar>
2023-12-08 16:43:26 -0300

    add zuul-microservice

---
+++ zuul-microservice/src/test/java/com/axioma/zuulmicroservice/ZuulMicroserviceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.axioma.zuulmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+@SpringBootTest
+class ZuulMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
